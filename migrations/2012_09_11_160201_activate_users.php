aa735604808d4180c70cb400294a93ca94619053
Roald Dijkstra <roald.dijkstra@babbletics.nl>
2012-09-12 11:52:05 +0200

    setting new user data and metadata / removing user columns

--- migrations/2012_09_11_160201_activate_users.php
+++ migrations/2012_09_11_160201_activate_users.php
@@ -9,12 +9,21 @@ class Sentry_Activate_Users {
 	 */
 	public function up()
 	{
+        // Password requires at least 80 characters for Sentry's hashing function
+        DB::query("ALTER TABLE  `users` CHANGE  `password`  `password` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;");
+
 		// Activate all existing users
 		$users = User::all();
 		$metadata = array();
 		foreach ($users as $user) {
 			$user->activated = 1;
 			$user->status = 1;
+
+			$salt = Str::random(16);
+			$password = "idd-". $user->username;
+			$password = $salt . hash('sha256', $salt.$password);
+			$user->password = $password;
+
 			$user->save();
 
 			$metadata[] = array(
@@ -27,7 +36,7 @@ class Sentry_Activate_Users {
 		DB::table('users_metadata')->insert($metadata);
 
         Schema::table('users', function($table) {
-        	$table->drop_column(array('first_name', 'last_name', 'middle_name', 'role'));
+        	$table->drop_column(array('first_name', 'last_name', 'middle_name', 'role', 'active'));
         });
 	}
 
